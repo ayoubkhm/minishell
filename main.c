c7ae4d8aaf68f78f05071ef8b0c30da8304d5bd8
Ayoub Khamassi <ayoub.khm@hotmail.fr>
2024-10-27 08:05:18 +0100

    corrected env expansion in parsing2

--- main.c
+++ main.c
@@ -18,6 +18,43 @@ void sigquit_handler(int sig_num)
     (void)sig_num;
 }
 
+char *ft_strndup(const char *s, size_t n)
+{
+    char *result = malloc(n + 1);
+    if (!result)
+        return NULL;
+    strncpy(result, s, n);
+    result[n] = '\0';
+    return result;
+}
+
+
+t_env_var *init_env(char **envp) {
+    t_env_var *env_list = NULL;
+    t_env_var *current = NULL;
+
+    for (int i = 0; envp[i]; i++) {
+        char *equal_sign = strchr(envp[i], '=');
+        if (equal_sign) {
+            size_t name_len = equal_sign - envp[i];
+            char *name = ft_strndup(envp[i], name_len);
+            char *value = strdup(equal_sign + 1);
+
+            t_env_var *new_var = malloc(sizeof(t_env_var));
+            new_var->name = name;
+            new_var->value = value;
+            new_var->next = NULL;
+
+            if (!env_list)
+                env_list = new_var;
+            else
+                current->next = new_var;
+
+            current = new_var;
+        }
+    }
+    return env_list;
+}
 
 
 int main(int argc, char **argv, char **envp)
@@ -26,6 +63,8 @@ int main(int argc, char **argv, char **envp)
     t_token     *tokens;
     t_cmd_list  *cmd_list;
     t_data      data;
+    t_env_var   *env_list;
+
 
     data.ac = argc;
     data.av = ft_copytab(argv);
@@ -36,6 +75,9 @@ int main(int argc, char **argv, char **envp)
 
     signal(SIGINT, sigint_handler);
     signal(SIGQUIT, sigquit_handler);
+
+    env_list = init_env(envp);
+
 
     while (1)
     {
@@ -55,7 +97,7 @@ int main(int argc, char **argv, char **envp)
         //print_tokens(tokens);
         if (check_syntax(tokens) == 0)
         {
-            cmd_list = parse_commands(tokens);
+            cmd_list = parse_commands(tokens, &env_list);
             if (!cmd_list)
             {
                 free_tokens(tokens);
