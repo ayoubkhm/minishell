c5e891d92f2f06409e97dbc9461465bb6216e9fa
Debian <debian@ov-7cb7be>
2024-11-24 19:00:10 +0100

    Minishell Map : Gestion des crochets

--- parsing/parsing_tokenizer.c
+++ parsing/parsing_tokenizer.c
@@ -1,5 +1,49 @@
 #include "parsing.h"
 
+int handle_brackets(char *input, int i, t_token **tokens, t_env *env_list)
+{
+    int start = ++i; // Avance après le crochet ouvrant '['
+    char *value;
+
+    while (input[i] && input[i] != ']') // Recherche le crochet fermant ']'
+        i++;
+
+    // Si le crochet fermant n'est pas trouvé, traite tout comme un texte brut
+    if (!input[i])
+    {
+        fprintf(stderr, "minishell: unmatched '[' treated as text\n");
+        value = ft_strndup(&input[start - 1], i - start + 1); // Inclut '['
+    }
+    else
+    {
+        // Extrait la chaîne entre les crochets
+        value = ft_strndup(&input[start], i - start);
+
+        // Effectue l'expansion des variables sur le contenu
+        char *expanded_value = expand_variables(value, env_list);
+        free(value);
+
+        // Reconstruit le token avec les crochets inclus
+        value = ft_strjoin("[", expanded_value);
+        free(expanded_value);
+
+        char *temp = value;
+        value = ft_strjoin(value, "]");
+        free(temp);
+
+        i++; // Avance après le crochet fermant ']'
+    }
+
+    // Ajoute le token
+    add_token(tokens, create_token(value, TYPE_WORD, 0));
+    free(value);
+
+    return i;
+}
+
+
+
+
 /**
  * Gère les guillemets simples et doubles.
  */
@@ -12,7 +56,7 @@ int handle_quotes(char *input, int i, t_token **tokens, t_env *env_list)
     while (input[i] && input[i] != quote_char)
         i++;
 
-    if (!input[i])
+    if (!input[i]) // Si la quote fermante n'est pas trouvée
     {
         fprintf(stderr, "minishell: syntax error: unclosed quote\n");
         return -1;
@@ -20,8 +64,13 @@ int handle_quotes(char *input, int i, t_token **tokens, t_env *env_list)
 
     value = ft_strndup(&input[start], i - start);
 
+    // Gère l'expansion des variables dans les guillemets doubles uniquement
     if (quote_char == '"')
-        value = expand_variables(value, env_list);
+    {
+        char *expanded_value = expand_variables(value, env_list);
+        free(value);
+        value = expanded_value;
+    }
 
     add_token(tokens, create_token(value, TYPE_WORD, quote_char == '"'));
     free(value);
@@ -34,7 +83,7 @@ int handle_quotes(char *input, int i, t_token **tokens, t_env *env_list)
  */
 int handle_backslash(char *input, int i, char **current_value)
 {
-    if (!input[i + 1])
+    if (!input[i + 1]) // Vérifie si le backslash est à la fin
     {
         fprintf(stderr, "minishell: syntax error: backslash at end of input\n");
         return -1;
@@ -45,9 +94,8 @@ int handle_backslash(char *input, int i, char **current_value)
     *current_value = ft_strjoin(*current_value, escaped_char);
     free(temp);
 
-    return i + 2;
+    return i + 2; // Avance après le backslash et le caractère suivant
 }
-
 
 /**
  * Gère une séquence de caractères normaux jusqu'à un opérateur ou espace.
@@ -60,7 +108,7 @@ int handle_regular_characters(char *input, int i, t_token **tokens)
 
     while (input[i] && !isspace(input[i]) && !is_operator(input[i]))
     {
-        if (input[i] == '\\')
+        if (input[i] == '\\') // Gère le backslash
         {
             i = handle_backslash(input, i, &current_value);
             if (i == -1)
@@ -69,13 +117,11 @@ int handle_regular_characters(char *input, int i, t_token **tokens)
                 return -1;
             }
         }
-        else if (input[i] == '$')
+        else if (input[i] == '$') // Interrompt pour gérer les variables
         {
-            // Handle variable expansion if needed
-            // For escaped $, it won't reach here
-            break;
+            break; // Le traitement du `$` sera géré ailleurs
         }
-        else
+        else // Ajoute le caractère au token courant
         {
             char temp_str[2] = {input[i], '\0'};
             char *temp = current_value;
@@ -85,13 +131,12 @@ int handle_regular_characters(char *input, int i, t_token **tokens)
         }
     }
 
-    if (*current_value)
+    if (*current_value) // Ajoute le token s'il n'est pas vide
         add_token(tokens, create_token(current_value, TYPE_WORD, 0));
     free(current_value);
 
     return i;
 }
-
 
 /**
  * Gère les opérateurs comme '|', '>' ou '<'.
@@ -161,25 +206,37 @@ t_token *tokenize_input(char *input, t_env *env_list)
 
     while (input[i])
     {
-        if (isspace(input[i]))
+        if (isspace(input[i])) // Ignore les espaces
         {
             i++;
             continue;
         }
-        else if (input[i] == '\'' || input[i] == '"')
+        else if (input[i] == '\'') // Gère les guillemets simples
         {
             i = handle_quotes(input, i, &tokens, env_list);
         }
-        else if (is_operator(input[i]))
+        else if (input[i] == '"') // Gère les guillemets doubles
+        {
+            i = handle_quotes(input, i, &tokens, env_list);
+        }
+        else if (input[i] == '$') // Gère les variables
+        {
+            i = handle_variable_reference(input, i, &tokens, env_list);
+        }
+        else if (input[i] == '[') // Gère les crochets
+        {
+            i = handle_brackets(input, i, &tokens, env_list);
+        }
+        else if (is_operator(input[i])) // Gère les opérateurs
         {
             i = handle_operator(input, i, &tokens);
         }
-        else
+        else // Gère les caractères normaux
         {
             i = handle_regular_characters(input, i, &tokens);
         }
 
-        if (i == -1)
+        if (i == -1) // Erreur détectée
         {
             free_tokens(tokens);
             return NULL;
@@ -189,26 +246,3 @@ t_token *tokenize_input(char *input, t_env *env_list)
     return tokens;
 }
 
-
-/**
- * Gère les guillemets dans les mots.
- */
-int handle_quotes_in_word(char *input, int i, t_token **tokens, char quote_char, int expand)
-{
-    int start = ++i;
-
-    while (input[i] && input[i] != quote_char)
-        i++;
-
-    if (!input[i])
-    {
-        fprintf(stderr, "minishell: syntax error: unclosed quote\n");
-        return -1;
-    }
-
-    char *value = ft_substr(input, start, i - start);
-    add_token(tokens, create_token(value, TYPE_WORD, expand));
-    free(value);
-
-    return i + 1;
-}
