4062132294d191ab48f6c41b927473aff7f0e032
Debian <debian@ov-7cb7be>
2024-12-10 00:25:45 +0100

    Ameloiration heredoc

--- parsing/heredoc_utils.c
+++ parsing/heredoc_utils.c
@@ -6,64 +6,41 @@
 /*   By: akhamass <akhamass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 01:47:25 by akhamass          #+#    #+#             */
-/*   Updated: 2024/12/08 14:34:02 by akhamass         ###   ########.fr       */
+/*   Updated: 2024/12/10 00:02:27 by akhamass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
+#include <dirent.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
-int	create_unique_temp_file(const char *base_filename, char *filename)
+
+
+
+int write_to_temp_file(int fd, const char *filename, const char *content)
 {
-	int	fd;
-	int	suffix;
-
-	fd = -1;
-	suffix = 0;
-	while (1)
-	{
-		printf(filename, "%s%d", base_filename, suffix);
-		fd = open(filename, O_CREAT | O_EXCL | O_RDWR, 0600);
-		if (fd != -1)
-			break ;
-		if (errno != EEXIST)
-		{
-			perror("Erreur de création du fichier temporaire");
-			return (-1);
-		}
-		suffix++;
-	}
-	return (fd);
-}
-
-char	*allocate_filename(const char *base_filename)
-{
-	char	*filename;
-
-	filename = malloc(strlen(base_filename) + 10);
-	if (!filename)
-		perror("Erreur d'allocation de mémoire pour le nom du fichier");
-	return (filename);
-}
-
-int	write_to_temp_file(int fd, const char *filename, const char *content)
-{
-	if (write(fd, content, strlen(content)) == -1)
-	{
-		perror("Erreur d'écriture dans le fichier temporaire");
-		unlink(filename);
-		return (-1);
-	}
-	return (0);
+    fprintf(stderr, "[DEBUG] Writing to file '%s':\n%s\n", filename, content);
+    if (write(fd, content, strlen(content)) == -1)
+    {
+        perror("Erreur d'écriture dans le fichier temporaire");
+        unlink(filename);
+        return (-1);
+    }
+    return (0);
 }
 
 char	*strjoin_with_newline(char *s1, char *s2)
 {
-	char	*result;
 	size_t	total_len;
 	size_t	len1;
 
 	total_len = calculate_total_length(s1, s2);
-	result = malloc(total_len);
+	char *result = malloc(total_len);
 	if (!result)
 		return (NULL);
 	len1 = 0;
@@ -84,11 +61,8 @@ char	*strjoin_with_newline(char *s1, char *s2)
 
 size_t	calculate_total_length(char *s1, char *s2)
 {
-	size_t	len1;
-	size_t	len2;
-
-	len1 = 0;
-	len2 = 0;
+	size_t	len1 = 0;
+	size_t	len2 = 0;
 	if (s1)
 		len1 = strlen(s1);
 	if (s2)
