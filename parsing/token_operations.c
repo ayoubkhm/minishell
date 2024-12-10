47bd0500a6f69ca803844221f468ff2979b83c37
Debian <debian@ov-7cb7be>
2024-12-10 02:42:07 +0100

    Correction variables collées

--- parsing/token_operations.c
+++ parsing/token_operations.c
@@ -6,7 +6,7 @@
 /*   By: akhamass <akhamass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 02:02:46 by akhamass          #+#    #+#             */
-/*   Updated: 2024/12/07 19:54:35 by akhamass         ###   ########.fr       */
+/*   Updated: 2024/12/10 00:47:08 by akhamass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,39 @@ int	gere_valid_var(t_ctx *ctx)
 	char	**split_values;
 	int		i;
 
+	printf("[DEBUG] gere_valid_var: start, ctx->i=%d, ctx->inp + ctx->i='%s'\n", ctx->i, ctx->inp + ctx->i);
+
 	i = ctx->i;
 	split_values = xtrct_split_var_val(ctx->inp, &i, ctx->e_l, ctx->dol_seq);
 	if (!split_values)
+	{
+		printf("[DEBUG] gere_valid_var: xtrct_split_var_val a échoué, retour i=%d\n", i);
 		return (i);
+	}
+	printf("[DEBUG] gere_valid_var: split_values extrait avec succès.\n");
+	// On peut debuguer le contenu de split_values
+	{
+		int j = 0;
+		while (split_values[j])
+		{
+			printf("[DEBUG] split_values[%d] = '%s'\n", j, split_values[j]);
+			j++;
+		}
+	}
+
 	i = add_all_tokens(ctx, split_values, i);
+	printf("[DEBUG] gere_valid_var: add_all_tokens retourne i=%d\n", i);
 	if (i == -1)
+	{
+		printf("[DEBUG] gere_valid_var: add_all_tokens a échoué.\n");
 		return (-1);
+	}
+
 	free_split_values(split_values);
+	printf("[DEBUG] gere_valid_var: fin normale, retour i=%d\n", i);
 	return (i);
 }
+
 
 int	cree_et_add_tok_lit(t_ctx *ctx, int start, int end)
 {
